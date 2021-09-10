050357ebfb48d6f3ecb2a7a07b0792962b17728a
David Wu <david204code@gmail.com>
2021-09-10 23:23:40 +0100

    Map the data and displaying the data roughly on screen

--- src/Belta.tsx
+++ src/Belta.tsx
@@ -4,16 +4,16 @@ const url = "https://api.github.com/repositories";
 
 const Belta = () => {
   // state value
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
 
   // fetch function
   const fetchUsers = async () => {
     const response = await fetch(url);
     // turn data into json method
-    const repos = await response.json();
-    // console.log(repos);
-    setRepos(repos);
+    const data = await response.json();
+    // console.log(data);
+    setRepos(data);
     setLoading(false);
   };
 
@@ -21,18 +21,34 @@ const Belta = () => {
     fetchUsers();
   }, []);
 
+  // setup the return condition
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  //  It should have owner profile picture on the first column, then owner name, repo name, repo url, and description
+  //  It should have a search feature and a pagination feature
+  //  It should be written using typescript and scss
+
   return (
     <>
       <h1>Repos</h1>
       {repos.map((repo) => {
-        const { id, name, description, owner } = repo;
-        console.log(repo);
+        const { id, name, html_url, owner, description } = repo;
+        // console.log(repo);
+        // console.log(owner);
         return (
-          <li key={id}>
-            <p>
-              {name}, {description}, {owner}
-            </p>
-          </li>
+          <h1 key={id}>
+            <p>Owner profile picture: {owner.avatar_url}</p>
+            <p>Owner name: {owner.login}</p>
+            <p>Repo name: {name}</p>
+            <p>Repo url: {html_url}</p>
+            <p>Descripition: {description}</p>
+          </h1>
         );
       })}
     </>
